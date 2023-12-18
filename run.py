d5f391214d7b029a63b47bd453832f30c34669b8
Jonah Nichols <jonah@Jonahs-MBP.lan>
2023-12-17 22:24:36 -0500

    Add conversation skeleton

--- run.py
+++ run.py
@@ -3,5 +3,9 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/conversation')
+def conversation():
+    return render_template('convseration-ai.html')
 if __name__ == '__main__':
     app.run(debug=True)
